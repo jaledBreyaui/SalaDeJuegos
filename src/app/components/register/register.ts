dd5388f953472aa014dddd839d2c5b16bd38ef52
Jaled <jaledbreyaui41@gmail.com>
2026-05-03 23:30:50 -0300

    register

--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, RouterLink, InputGroupModule, InputGroupAddonModule, InputTextModule, ButtonModule],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  usuario = '';
+  password = '';
+  nombre: string = '';
+  apellido: string = '';
+  edad: number = 0;
+  correo: string = '';
+}
