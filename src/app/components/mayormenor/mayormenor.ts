f9c7b4610d20a1ffc0fe4bc67e4bdd178d6cab51
Jaled <jaledbreyaui41@gmail.com>
2026-05-13 23:41:53 -0300

    mayormenor avance

--- src/app/components/mayormenor/mayormenor.ts
+++ src/app/components/mayormenor/mayormenor.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Deck } from '../../services/deck/deck';
-
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { NgClass } from "@angular/common";
 @Component({
   selector: 'app-mayormenor',
-  imports: [],
+  imports: [ButtonModule, NgClass],
   templateUrl: './mayormenor.html',
   styleUrl: './mayormenor.css',
 })
@@ -11,21 +13,51 @@ export class Mayormenor {
   valorCarta: number = 0
   imagenCarta = signal('')
   valorCartaPrevia: number = 0
+  puntaje = signal(0)
+  juegoPerdido = signal(false)
+  eleccionJugador = signal('')
+  mostrarAnimacionError = signal(false)
+  mostrarAnimacionAcierto = signal(false)
+
   constructor(private deck: Deck) { }
 
   ngOnInit() {
     this.darCarta()
   }
 
-  darCarta() {
+  darCarta(eleccion?: string) {
     this.deck.getCards().subscribe((data) => {
       if (data) {
-        console.log(data)
+        this.valorCartaPrevia = this.valorCarta
         this.imagenCarta.set(data.cards[0].image);
         this.valorCarta = this.verificarValor(data.cards[0].value)
-        console.log(this.valorCarta);
+
+        if (eleccion === 'mayor' && this.valorCarta <= this.valorCartaPrevia) {
+          this.juegoPerdido.set(true)
+          this.mostrarAnimacionError.set(true)
+        }
+
+        if (eleccion === 'menor' && this.valorCarta >= this.valorCartaPrevia) {
+          this.juegoPerdido.set(true)
+          this.mostrarAnimacionError.set(true)
+        }
+
+        if (this.valorCartaPrevia > 0) {
+
+          this.puntaje.set(this.puntaje() + 250)
+        }
+
+
       }
     });
+  }
+
+  elijeMayor() {
+    this.darCarta('mayor')
+  }
+
+  elijeMenor() {
+    this.darCarta('menor')
   }
 
   verificarValor(value: string): number {
