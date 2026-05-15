e746a311c26c8c33cd9d7d7e71aedc31a4cc9996
Nicolás Breyaui <nbreyaui@apn.gob.ar>
2026-05-15 16:29:46 -0300

    porlas

--- src/app/components/ahorcado/ahorcado.ts
+++ src/app/components/ahorcado/ahorcado.ts
@@ -7,7 +7,6 @@ import { Router } from '@angular/router';
 import { Supabase } from '../../services/supabase/supabase';
 import { User } from '@supabase/supabase-js';
 
-
 @Component({
   selector: 'app-ahorcado',
   imports: [Contador, DialogModule, ButtonModule],
@@ -17,38 +16,41 @@ import { User } from '@supabase/supabase-js';
 export class Ahorcado implements OnInit {
   palabras = PALABRAS;
   palabraSecreta = signal<string>('');
-  letrasElegidas = signal<string[]>([])
+  letrasElegidas = signal<string[]>([]);
   errores = signal(7);
-  ganar = signal(false)
-  juegoPerdido = signal(false)
-  puntaje = signal(0)
+  ganar = signal(false);
+  juegoPerdido = signal(false);
+  puntaje = signal(0);
   visible = signal(false);
   mensajeFinDejuego = '';
-  dataUsuario: User | undefined
+  dataUsuario: User | undefined;
   mostrarPalabra = computed(() => {
     return this.palabraSecreta()
       .split('')
-      .map(letra => this.letrasElegidas().includes(letra) ? letra : '_');
+      .map((letra) => (this.letrasElegidas().includes(letra) ? letra : '_'));
   });
 
   imagenAhorcado = computed(() => {
     return `media/juegos/ahorcado/ahorcado-${this.errores()}.png`;
   });
 
-  constructor(private router: Router, private sb: Supabase) { }
+  constructor(
+    private router: Router,
+    private sb: Supabase,
+  ) {}
 
   ngOnInit() {
     this.sortearPalabra();
   }
 
   elegirLetra(letra: string) {
-    this.letrasElegidas.update(letras => [...letras, letra]);
+    this.letrasElegidas.update((letras) => [...letras, letra]);
     if (!this.palabraSecreta().includes(letra)) {
-      this.errores.update(errores => errores - 1)
+      this.errores.update((errores) => errores - 1);
     } else {
-      this.puntaje.set(this.puntaje() + 100)
+      this.puntaje.set(this.puntaje() + 100);
     }
-    this.verificarEstado()
+    this.verificarEstado();
   }
 
   sortearPalabra() {
@@ -58,41 +60,39 @@ export class Ahorcado implements OnInit {
   }
 
   verificarEstado() {
-    const palabraCompleta = this.mostrarPalabra().join('')
+    const palabraCompleta = this.mostrarPalabra().join('');
     if (palabraCompleta == this.palabraSecreta()) {
-      this.ganar.set(true)
-      this.puntaje.set(this.puntaje() + (this.errores() * 100))
-      this.mensajeFinDejuego = `Adivinaste!`
-      this.showDialog()
+      this.ganar.set(true);
+      this.puntaje.set(this.puntaje() + this.errores() * 100);
+      this.mensajeFinDejuego = `Adivinaste!`;
+      this.showDialog();
     }
     if (this.errores() == 0) {
-      this.juegoPerdido.set(true)
-      this.mensajeFinDejuego = `Perdiste!`
-      this.showDialog()
+      this.juegoPerdido.set(true);
+      this.mensajeFinDejuego = `Perdiste!`;
+      this.showDialog();
     }
-
   }
 
-  reiniciarJuego() {
+  reiniciarJuego = () => {
     if (!this.ganar()) {
-      this.puntaje.set(0)
+      this.puntaje.set(0);
     }
     this.errores.set(7);
-    this.sortearPalabra()
-    this.letrasElegidas.set([])
+    this.sortearPalabra();
+    this.letrasElegidas.set([]);
     this.ganar.set(false);
     this.juegoPerdido.set(false);
-    this.closeDialog()
-  }
+    this.closeDialog();
+  };
 
-  abandonarjuego() {
+  abandonarjuego = () => {
     if (this.puntaje() > 0) {
-      this.sb.guardarPuntajes('ahorcado', this.puntaje())
+      this.sb.guardarPuntajes('ahorcado', this.puntaje());
     }
     this.router.navigate(['/home']);
-    this.closeDialog()
-  }
-
+    this.closeDialog();
+  };
 
   ////Modal
   showDialog() {
@@ -102,5 +102,4 @@ export class Ahorcado implements OnInit {
   closeDialog() {
     this.visible.set(false);
   }
-
 }
